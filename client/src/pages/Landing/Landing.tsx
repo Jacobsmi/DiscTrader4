b1f55a41971622b80f4923578d40ac9926c54d92
Jacobsmi <Jacobsm84@gmail.com>
2021-11-16 19:14:52 -0500

    Landing Page created successfully

---
+++ client/src/pages/Landing/Landing.tsx
@@ -0,0 +1,25 @@
+import "./Landing.css";
+import disc from "./FlyingDisc.svg";
+import { Link } from "react-router-dom";
+
+const Landing = () => {
+  return (
+    <div className="Landing">
+      <div className="Landing-Body">
+        <div className="Landing-Body-Main">Disc Trader</div>
+        <div className="Landing-Body-Sub">
+          The best place to exchange anything disc golf.
+        </div>
+      </div>
+      <div className="Landing-Image">
+        <img src={disc} alt="Flying Disc" className="Landing-Image-Src"/>
+      </div>
+      <div className="Landing-Header">
+        <Link to="/signup" className="Landing-Header-Button">Get Started</Link>
+        <Link to="/signin" className="Landing-Header-SignIn">Sign In</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
