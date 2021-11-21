b6a494e3dff2a0bcd799405c0f147a9196f8c52c
Michael Jacobs <jacobsm84@gmail.com>
2021-11-20 20:23:32 -0500

    Desktop Styling created

--- client/src/pages/Signin/Signin.tsx
+++ client/src/pages/Signin/Signin.tsx
@@ -3,6 +3,12 @@ import disc from "../../imgs/FlyingDisc.svg";
 import { Link } from "react-router-dom";
 
 const Signin = () => {
+
+  const processSignin = () => {
+    console.log("Processing Sign in");
+    // Needs the database
+  }
+
   return (
     <div className="Signin">
       <div className="Signin-img">
@@ -24,7 +30,7 @@ const Signin = () => {
         </div>
       </div>
       <div className="Signin-Button-Container">
-        <button className="Signin-Button">Sign In</button>
+        <button className="Signin-Button" onClick={processSignin}>Sign In</button>
         <span>Not a member yet? <Link className="Signin-Link" to="/signup">Sign Up Now</Link></span>
       </div>
     </div>
